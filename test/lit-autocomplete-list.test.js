f84f01b4ed42f4c6f7560dd9c94d869b72133d4e
andreluiz.dsn.pack <andreluiz.dsn.pack@alterdata.com.br>
2024-11-07 14:41:18 -0300

    inicio do arquivo de teste autocomplete list

---
+++ test/lit-autocomplete-list.test.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+import { html } from 'lit'
+import { fixture, expect } from '@open-wc/testing'
+import '../lit-autocomplete-list.js'
