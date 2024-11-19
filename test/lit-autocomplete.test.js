6c4408a4af55684079fd706c94cd43a673bb7f31
andreluiz.dsn.pack <andreluiz.dsn.pack@alterdata.com.br>
2024-11-19 09:02:45 -0300

    package json tem os globals do open-wc testing para o standard

--- test/lit-autocomplete.test.js
+++ test/lit-autocomplete.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
 import { html } from 'lit'
 import { fixture, expect } from '@open-wc/testing'
 import '../lit-autocomplete.js'
